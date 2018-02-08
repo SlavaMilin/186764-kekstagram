a65f65615975ad06813db4a905974f9e6d21a14a
SlavaMilin <slava.milin@gmail.com>
2018-02-08 20:39:25 +0300

    исправляет прошлый пуллреквест

--- js/pictures.js
+++ js/pictures.js
@@ -1,16 +1,21 @@
 'use strict';
-var PICTURE_ITERATION = 25;
+var PICTURE = {
+  ITERATION: 25,
+  DIR: 'photos/',
+  EXTENSION: '.jpg',
+  COMMENTS: [
+    'Всё отлично!',
+    'В целом всё неплохо. Но не всё.',
+    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  ]
+};
+
 var template = document.querySelector('#picture-template').content;
 var pasteInto = document.querySelector('.pictures');
 var gallery = document.querySelector('.gallery-overlay');
-var COMMENTS = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-];
 
 var getRandomArrange = function (min, max) {
   return Math.round(Math.random() * (max - min) + min);
@@ -18,17 +23,17 @@ var getRandomArrange = function (min, max) {
 
 var getRandomComment = function (data) {
   var result = [];
-  for (var i = 0; i < getRandomArrange(1, 2); i += 1) {
+  for (var i = 0; i < getRandomArrange(1, 2); i++) {
     result.push(data.pop([getRandomArrange(0, data.length - 1)]));
   }
   return result;
 };
 
-var addPictureData = function (iteration, text) {
+var addPictureData = function (count, text) {
   var pictures = [];
-  for (var i = 0; i < iteration; i++) {
+  for (var i = 0; i < count; i++) {
     pictures.push({
-      url: 'photos/' + (i + 1) + '.jpg',
+      url: PICTURE.DIR + (i + 1) + PICTURE.EXTENSION,
       likes: getRandomArrange(15, 185),
       comments: getRandomComment(text)
     });
@@ -36,9 +41,9 @@ var addPictureData = function (iteration, text) {
   return pictures;
 };
 
-var printTemplate = function (iteration, data) {
+var printTemplate = function (count, data) {
   var fragment = document.createDocumentFragment();
-  for (var i = 0; i < iteration; i += 1) {
+  for (var i = 0; i < count; i += 1) {
     var pictureElement = template.cloneNode(true);
     pictureElement.querySelector('img').src = data[i].url;
     pictureElement.querySelector('.picture-likes').textContent = data[i].likes;
@@ -49,13 +54,13 @@ var printTemplate = function (iteration, data) {
 };
 
 var generateGallery = function (obj, data) {
-  obj.classList.remove('hidden');
-  obj.querySelector('.gallery-overlay-image').src = data[0].url;
-  obj.querySelector('.likes-count').textContent = data[0].likes;
-  obj.querySelector('.comments-count').textContent = data[0].comments.length;
+  var testData = data[0];
+  obj.querySelector('.gallery-overlay-image').src = testData.url;
+  obj.querySelector('.likes-count').textContent = testData.likes;
+  obj.querySelector('.comments-count').textContent = testData.comments.length;
 };
 
-var pictureData = addPictureData(PICTURE_ITERATION, COMMENTS);
-printTemplate(PICTURE_ITERATION, pictureData);
-generateGallery(gallery, pictureData);
+var picturesData = addPictureData(PICTURE.ITERATION, PICTURE.COMMENTS);
+printTemplate(PICTURE.ITERATION, picturesData);
+generateGallery(gallery, picturesData);
 
