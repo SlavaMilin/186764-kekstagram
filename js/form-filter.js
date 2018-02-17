03df7c68064fb7cc7aabfe5a39bc21b366a25c4d
Slava <slava.milin@gmail.com>
2018-02-17 03:04:49 +0300

    добавляет фильтры

--- js/form-filter.js
+++ js/form-filter.js
@@ -7,6 +7,7 @@
   var effectsBtn = uploadForm.querySelectorAll('input[name="effect"]');
   var mainFilterImage = uploadForm.querySelector('.effect-image-preview');
   var slider = uploadForm.querySelector('.upload-effect-level');
+  var pin = uploadForm.querySelector('.upload-effect-level-pin');
 
   slider.classList.add('hidden');
 
@@ -18,6 +19,27 @@
     }
     mainFilterImage.className = EFFECT_PREFIX + evt.target.value;
     mainFilterImage.classList.add('effect-image-preview');
+  };
+
+  var onPinMouseDown = function (evt) {
+    evt.preventDefault();
+
+    var startX = evt.clientX;
+
+    var onMouseMove = function (moveEvt) {
+      var shiftX = startX - moveEvt.clientX;
+      startX = moveEvt.clientX;
+      pin.style.left = (pin.offsetLeft - shiftX);
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
   };
 
   effectsBtn.forEach(function (el) {
